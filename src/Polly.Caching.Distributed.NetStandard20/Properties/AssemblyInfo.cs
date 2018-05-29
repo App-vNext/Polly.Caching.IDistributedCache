964f71865603037e752a673429e414dc4007ebf3
reisenberger <software@reisenberger.net>
2018-05-29 12:52:42 +0100

    Bump version to v2.0.0

--- src/Polly.Caching.Distributed.NetStandard20/Properties/AssemblyInfo.cs
+++ src/Polly.Caching.Distributed.NetStandard20/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Polly.Caching.Distributed")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("1.0.5.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
 [assembly: CLSCompliant(false)] // Because Microsoft.Extensions.Caching.Memory.IDistributedCache, on which Polly.Caching.IDistributedCache.NetStandard11 depends, is not CLSCompliant.
 
 [assembly: InternalsVisibleTo("Polly.Caching.Distributed.NetStandard20.Specs")]
