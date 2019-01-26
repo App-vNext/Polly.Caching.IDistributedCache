18572d02691506b89232e1de6a6556ac8f188366
reisenberger <software@reisenberger.net>
2019-01-26 10:54:27 +0000

    Refactor specs

---
+++ src/Polly.Caching.Distributed.SharedSpecs/Integration/CachePolicyFactory.cs
@@ -0,0 +1,37 @@
+﻿#if !NETCOREAPP1_1
+
+using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Polly.Caching.Serialization.Json;
+
+namespace Polly.Caching.Distributed.Specs.Integration
+{
+    public class CachePolicyFactory
+    {
+        public static (ISyncCacheProvider<TResult>, ISyncPolicy<TResult>) CreateCachePolicy<TCache, TResult>()
+        {
+            var memoryIDistributedCache = new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions()));
+            ISyncCacheProvider<TResult> memoryIDistributedCacheProvider;
+            if (typeof(TCache) == typeof(string))
+            {
+                memoryIDistributedCacheProvider = memoryIDistributedCache.AsSyncCacheProvider<string>().WithSerializer<TResult, string>(new JsonSerializer<TResult>(new JsonSerializerSettings()));
+            }
+            else if (typeof(TCache) == typeof(byte[]))
+            {
+                memoryIDistributedCacheProvider = memoryIDistributedCache.AsSyncCacheProvider<byte[]>().WithSerializer(new ByteArraySerializer()).WithSerializer<TResult, string>(new JsonSerializer<TResult>(new JsonSerializerSettings()));
+            }
+            else
+            {
+                throw new ArgumentException($"{nameof(TCache)} must be either {typeof(string).Name} or {typeof(byte).Name}[]", nameof(TCache));
+            }
+
+            var policy = Policy.Cache<TResult>(memoryIDistributedCacheProvider, TimeSpan.FromHours(1));
+            return (memoryIDistributedCacheProvider, policy);
+        }
+    }
+}
+
+#endif
---
+++ src/Polly.Caching.Distributed.SharedSpecs/Integration/CacheRoundTripSpecsBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Xunit;
+
+namespace Polly.Caching.Distributed.Specs.Integration
+{
+    public abstract class CacheRoundTripSpecsBase
+    {
+        protected const string OperationKey = "SomeOperationKey";
+
+        public static TheoryData<SampleClass> SampleClassData =>
+            new TheoryData<SampleClass>
+            {
+                new SampleClass(),
+                new SampleClass()
+                {
+                    StringProperty = "<html></html>",
+                    IntProperty = 1
+                },
+                (SampleClass)null,
+                default(SampleClass)
+            };
+
+        public static TheoryData<String> SampleStringData =>
+            new TheoryData<String>
+            {
+                "some string",
+                "",
+                null,
+                default(string),
+                "null"
+            };
+
+        public static TheoryData<int> SampleNumericData =>
+            new TheoryData<int>
+            {
+                -1,
+                0,
+                1,
+                default(int)
+            };
+
+        public static TheoryData<SampleEnum> SampleEnumData =>
+            new TheoryData<SampleEnum>
+            {
+                SampleEnum.FirstValue,
+                SampleEnum.SecondValue,
+                default(SampleEnum),
+            };
+
+        public static TheoryData<bool> SampleBoolData =>
+            new TheoryData<bool>
+            {
+                true,
+                false,
+                default(bool),
+            };
+
+        public static TheoryData<bool?> SampleNullableBoolData =>
+            new TheoryData<bool?>
+            {
+                true,
+                false,
+                null,
+                default(bool?),
+            };
+
+        public class SampleClass
+        {
+            public string StringProperty { get; set; }
+            public int IntProperty { get; set; }
+        }
+
+        public enum SampleEnum
+        {
+            FirstValue,
+            SecondValue,
+        }
+    }
+}
